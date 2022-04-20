a6ec36dac71c2a7a8b679e27db75ae4848c054d1
acgotaku <acgotaku311@gmail.com>
2022-04-20 19:29:57 +0900

    feat: svg support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
@@ -14,7 +15,7 @@ export default defineConfig(async ({ mode }) => {
         '@': resolve(__dirname, 'src')
       }
     },
-    plugins: [react()],
+    plugins: [react(), svgr()],
     build: {
       sourcemap: mode === 'production' ? 'hidden' : true,
       emptyOutDir: true
