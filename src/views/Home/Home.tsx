b6c5c66ff74ffa4652c9f4ee28c08e152641415e
acgotaku <acgotaku311@gmail.com>
2022-04-21 21:06:22 +0900

    feat: home view

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from '@/components';
 import styles from './home.module.css';
 
 const HomeView = () => {
   return (
-    <div className={styles.home}>
-      {'home view'}
-      <Link to="/about">About</Link>
-    </div>
+    <article className={styles.home}>
+      <h1 className={styles.title}>Simple UI</h1>
+      <p className={styles.desc}>
+        Learn how to build a simple user interface from the soucre code.
+      </p>
+      <Link
+        className={styles.link}
+        href="https://github.com/acgotaku/simple-ui"
+      >
+        {' '}
+        Github{' '}
+      </Link>
+    </article>
   );
 };
 
