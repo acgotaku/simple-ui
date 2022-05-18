2fe3b0b41c96b1029e2588c4326520ef1996c6ad
acgotaku <acgotaku311@gmail.com>
2022-05-18 20:15:16 +0900

    feat: unit test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button', () => {
+  it('should render button', () => {
+    render(<Button>{'Standard Button'}</Button>);
+    expect(screen.getByRole('button')).toHaveTextContent('Standard Button');
+  });
+  it('should pass type to button', () => {
+    render(<Button type="submit" />);
+    expect(screen.getByRole('button')).toHaveAttribute('type', 'submit');
+  });
+  it('should render disabled button', () => {
+    render(<Button disabled />);
+    expect(screen.getByRole('button')).toBeDisabled();
+  });
+  it('should render loading button', () => {
+    const { container } = render(<Button loading />);
+    expect(container.querySelector('svg')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeDisabled();
+  });
+});
