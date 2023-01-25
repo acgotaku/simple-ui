60682bf358f92ce236611f100170e5dcfefc6191
acgotaku <acgotaku311@gmail.com>
2023-01-25 23:15:12 +0900

    fix: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '@/i18n';
 import App from '@/App';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
